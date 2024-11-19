1b82a86eaf2c0becf1f84890d8d6753fab937512
Katock-Cricket <787288968@qq.com>
2024-11-19 10:59:19 +0800

    更新设置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,14 +15,11 @@ export default [{
     },
 
     rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
+        "@typescript-eslint/naming-convention": ["off"],
 
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
+        curly: "off",
+        eqeqeq: "off",
+        "no-throw-literal": "off",
+        semi: "off",
     },
 }];
